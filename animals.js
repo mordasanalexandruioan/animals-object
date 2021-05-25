2b4a9d8a28c550d1fe7c7df6de957425fb2d0502
Mordasan Alexandru-Ioan <mordasanalex@yahoo.com>
2021-05-25 19:43:14 +0300

    tema object

---
+++ animals.js
@@ -0,0 +1,48 @@
+const animals = [{
+        name: 'Joey',
+        type: 'Dog',
+        breed: 'Golden Retriever',
+        age: 8,
+        photo: 'img/dog.jpeg'
+    },
+    {
+        name: 'Patches',
+        type: 'Cat',
+        breed: 'Domestic Shorthair',
+        age: 1,
+        photo: 'img/cat.jpg'
+    },
+    {
+        name: 'Kangsley',
+        type: 'Kangaroo',
+        breed: 'Australian Kangaroo',
+        age: 5,
+        photo: 'img/kangaroo.jpg'
+    },
+    {
+        name: 'Simba',
+        type: 'Elephant',
+        breed: 'Asiatic Elephant',
+        age: 10,
+        photo: 'img/elephant.jpg'
+    },
+];
+
+let pets = document.querySelector('.card')
+
+
+function displayAnimals() {
+    let text = ``;
+    for (let i = 0; i < animals.length; i++) {
+        text += `
+        <h2>${animals[i].name}</h2>
+            <h3>${animals[i].type} | ${animals[i].breed}</h3>
+            <p>Age: ${animals[i].age}</p>
+            <img src="${animals[i].photo}">
+            <hr>
+        `;
+
+    }
+    return text;
+}
+pets.innerHTML = displayAnimals();
